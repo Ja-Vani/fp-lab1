1f85070213154d48b24034fea657b27f8fccaa10
Ja_Vani <Sonic_Van@mail.ru>
2024-02-10 13:12:14 +0300

    fix lint

--- src/9/task1.erl
+++ src/9/task1.erl
@@ -6,24 +6,24 @@ special_pyth_trip(K)->
     special_pyth_trip(1, 1, math:pow(2, 0.5), K).
 
 special_pyth_trip(A, B, C, K) when A + B + C == K -> {A, B, C};
-special_pyth_trip(A, B, C, K) when A + B + C < K -> special_pyth_trip(A, B+1, math:pow(A*A + (B+1)*(B+1), 0.5), K);
-special_pyth_trip(A, B, C, K) when A + B > K -> {};
-special_pyth_trip(A, B, C, K)-> special_pyth_trip(A+1, A+1, math:pow(2*(A+1)*(A+1), 0.5), K).
+special_pyth_trip(A, B, C, K) when A + B + C < K -> special_pyth_trip(A, B+1, math:pow(A * A + (B + 1) * (B + 1), 0.5), K);
+special_pyth_trip(A, B, _, K) when A + B > K -> {};
+special_pyth_trip(A, _, _, K)-> special_pyth_trip(A+1, A+1, math:pow(2 * (A + 1) * (A + 1), 0.5), K).
 
 %rec
 special_pyth_trip_r(K)->
     special_pyth_trip_r(1, 1, K).
 
 special_pyth_trip_r(A, B, K)->
-    C = math:pow(A*A + B*B, 0.5),
+    C = math:pow(A * A + B * B, 0.5),
     if  A + B + C == K ->
             {A, B, C};
         A + B + C < K ->
-            special_pyth_trip_r(A, B+1, K);
+            special_pyth_trip_r(A, B + 1, K);
         A + B > K->
             {};
         A + B + C > K ->
-            special_pyth_trip_r(A+1, A+1, K)
+            special_pyth_trip_r(A + 1, A + 1, K)
     end.
 
 %fold
@@ -32,9 +32,9 @@ is_spicial(_, Acc)-> Acc.
 
 get_fold_special_list(K)-> get_fold_special_list(1, 1, math:pow(2, 0.5), K).
 
-get_fold_special_list(A, B, C, K) when B < K/2-> [{A, B, C, K}| get_fold_special_list(A, B+1, math:pow(A*A + (B+1)*(B+1), 0.5), K)];
-get_fold_special_list(A, B, C, K) when A < K/2-> [{A, B, C, K}| get_fold_special_list(A+1, A+1, math:pow(2*(A+1)*(A+1), 0.5), K)];
-get_fold_special_list(A, B, C, K) -> [].
+get_fold_special_list(A, B, C, K) when B < K/2-> [{A, B, C, K}| get_fold_special_list(A, B + 1, math:pow(A * A + (B + 1) * (B + 1), 0.5), K)];
+get_fold_special_list(A, B, C, K) when A < K/2-> [{A, B, C, K}| get_fold_special_list(A+1, A+1, math:pow(2 * (A + 1) * (A + 1), 0.5), K)];
+get_fold_special_list(_, _, _, _) -> [].
 
 special_pyth_trip_f(K)->
     lists:foldl(fun({A, B, C, K1}, Acc)->is_spicial({A, B, C, K1}, Acc) end, {},
--- test/9/task1_tests.erl
+++ test/9/task1_tests.erl
@@ -3,12 +3,12 @@
 
 % Reduce with tail recursion
 get_max_factor_test() ->
-    ?assertEqual(task1:special_pyth_trip(1000), {200,375,425.0}).
+    ?assertEqual(task1:special_pyth_trip(1000), {200, 375, 425.0}).
 
 % Reduce with recursion
 get_max_factor_t_test() ->
-    ?assertEqual(task1:special_pyth_trip_r(1000), {200,375,425.0}).
+    ?assertEqual(task1:special_pyth_trip_r(1000), {200, 375, 425.0}).
 
 % Fold
 get_max_factor_f_test() ->
-    ?assertEqual(task1:special_pyth_trip_f(1000), {200,375,425.0}).
+    ?assertEqual(task1:special_pyth_trip_f(1000), {200, 375, 425.0}).
